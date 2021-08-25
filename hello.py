17b949862a6e46367f90f24fb76c0ddf598c2e85
vivekabhupa97 <89552407+vivekabhupa97@users.noreply.github.com>
2021-08-26 05:24:31 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+num1= int(input("enter first num"))
+num2= int(input("enter second num"))
+
+rem = num1 % 2
+if(rem==0):
+    print(num1,"even integer")
+else:
+    print(num1,"odd integer")
+
+    
+        
